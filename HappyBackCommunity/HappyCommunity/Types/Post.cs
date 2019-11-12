4dd52d012fb5643829e6fff2a4aaae85c6ec45fe
Froziph <froziph@gmail.com>
2019-11-12 16:21:55 +0100

    getpost and getposts working

--- HappyBackCommunity/HappyCommunity/Types/Post.cs
+++ HappyBackCommunity/HappyCommunity/Types/Post.cs
@@ -5,13 +5,9 @@ using System.Threading.Tasks;
 
 namespace HappyCommunity.Types
 {
-	public class Post
+	public class Post : PostOverviewModel
 	{
-        public long Id { get; set; }
-        public string Title { get; set; }
-        public string Decription { get; set; }
-        public int Reward { get; set; }
-        public string Creator { get; set; }
-
+		public string Description { get; set; }
+		public int ZipCode { get; set; }
 	}
 }
