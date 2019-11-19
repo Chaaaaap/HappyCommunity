070b816003259d4c9e33d588733317475b3673ca
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2019-11-19 11:48:14 +0100

    Added PostOverviewModel

---
+++ HappyCommunity/HappyCommunityApp/app/src/main/java/com/app/happycommunity/models/PostOverviewModel.java
@@ -0,0 +1,63 @@
+package com.app.happycommunity.models;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PostOverviewModel {
+    private long id;
+    private String title;
+    private int reward;
+    private String creator;
+    private String city;
+
+    public PostOverviewModel(long id, String title, int reward, String creator, String city) {
+        this.id = id;
+        this.title = title;
+        this.reward = reward;
+        this.creator = creator;
+        this.city = city;
+    }
+    public PostOverviewModel(JSONObject object) {
+        try {
+            this.id = object.getLong("id");
+            this.title = object.getString("title");
+            this.reward = object.getInt("reward");
+            this.creator = object.getString("creator");
+            this.city = object.getString("city");
+        } catch(JSONException e) {
+            e.printStackTrace();
+            Log.d("DEBUG: ", e.getMessage());
+        }
+    }
+
+    public long getId() {
+        return this.id;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public int getReward() {
+        return this.reward;
+    }
+    public String getCreator() {
+        return this.creator;
+    }
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
