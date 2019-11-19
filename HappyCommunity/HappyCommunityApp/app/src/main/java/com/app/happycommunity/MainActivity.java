e55ca41df93ab0d671788ab1b5f54eb794425ca1
Mriis <mathias.riis.1@gmail.com>
2019-11-19 10:25:27 +0100

    switchToAndroid

---
+++ HappyCommunity/HappyCommunityApp/app/src/main/java/com/app/happycommunity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.app.happycommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        final EditText password = (EditText) findViewById(R.id.password);
+        final EditText userName = (EditText) findViewById(R.id.userName);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            
+            }
+        });
+    }
+
+}
